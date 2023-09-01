cf6242197ddc007002bf339ad65bd15055f4f0f6
Z4NR34L <software@zanreal.net>
2023-09-01 12:49:24 +0200

    Prepared WebView for workplace chat

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+fn main() -> wry::Result<()> {
+    use wry::{
+        application::{
+            event::{Event, StartCause, WindowEvent},
+            event_loop::{ControlFlow, EventLoop},
+            window::WindowBuilder,
+        },
+        webview::WebViewBuilder,
+    };
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("Workplace Chat")
+        .build(&event_loop)?;
+    let _webview = WebViewBuilder::new(window)?
+        .with_url("https://work.facebook.com/chat/")?
+        // .with_incognito(true)
+        .build()?;
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::NewEvents(StartCause::Init) => println!("Wry has started!"),
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            _ => (),
+        }
+    });
+}
